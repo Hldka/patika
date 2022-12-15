b44b571e14d4c20283aa74c97eed15a744f61782
Hldka <116027001+Hldka@users.noreply.github.com>
2022-12-15 17:30:28 +0100

    first commit

---
+++ src/main/java/kosulluifadelervekodbloklari/dnm.java
@@ -0,0 +1,15 @@
+package kosulluifadelervekodbloklari;
+
+public class dnm {
+    public static void main(String args[]) {
+        String x = "Patika.dev";
+        kodluyoruz(x);
+        System.out.println(x);
+    }
+    static void kodluyoruz(String y)
+    {
+        String x = "Java101";
+        y = "Kodluyoruz";
+    }
+}
+
