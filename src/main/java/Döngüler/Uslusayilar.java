dc8076724834f16d0d8a7c644108405a483db195
Hldka <116027001+Hldka@users.noreply.github.com>
2022-12-23 14:30:11 +0100

    first commit

---
+++ src/main/java/Döngüler/Uslusayilar.java
@@ -0,0 +1,19 @@
+package Döngüler;
+
+import java.util.Scanner;
+
+public class Uslusayilar {
+    public static void main(String[] args) {
+        int n,k ;
+        Scanner scan =new Scanner(System.in);
+        System.out.println(" bir sayi giriniz");
+        n=scan.nextInt();
+        System.out.println("Girmek istediginiz üslü sayiyiy yaziniz...");
+        k=scan.nextInt();
+        int sum=1;
+        for (int i=1;i<=k;i++){
+            sum=sum*n;
+        }
+        System.out.println("sonuc : "+sum);
+    }
+}
